f2aa75a2f21fd5baa79df6bca41684d41f8c1992
Siddharth Santurkar <ssanturk@andrew.cmu.edu>
2016-02-15 18:13:26 -0500

    Node binary search: half implemented

--- src/backend/index/bwtree.cpp
+++ src/backend/index/bwtree.cpp
@@ -15,7 +15,26 @@
 namespace peloton {
 namespace index {
 
-// Add your function definitions here
+	template <typename KeyType, typename ValueType, class KeyComparator>
+	int BWTree::find_lower(const DeltaChain& chain, const KeyType &key) {
+		// Assuming no delta records, the delta chain just contains
+		// a Node type (i.e. either inner or leaf node)
+		Node *node = reinterpret_cast<Node *>(chain.head);
 
+		//empty node? return index of 0th element
+		if(node->keys.size() == 0) return 0;
+
+		//set the binary search range
+		int min = 0, max = node->keys.size() - 1;
+
+		while (min < max) {
+			//find middle element
+			int mid = (min + max) >> 1;
+
+
+		}
+
+
+	}
 }  // End index namespace
 }  // End peloton namespace
