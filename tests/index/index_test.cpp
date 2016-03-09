1d82a64501e4732ac0581a6136ded61730530f98
Siddharth Santurkar <ssanturk@andrew.cmu.edu>
2016-03-09 16:03:13 -0500

    Fix: Insert retry; print tree

--- tests/index/index_test.cpp
+++ tests/index/index_test.cpp
@@ -390,7 +390,86 @@ namespace peloton {
     }
   }
 
-  TEST(IndexTests, SplitConsolidateTest) {
+  void SplitInsertIndexDeltaTest(index::Index *index, VarlenPool *pool, size_t scale_factor) {
+
+    // Loop based on scale factor
+    for (size_t scale_itr = 1; scale_itr <= scale_factor; scale_itr++) {
+      // Insert a bunch of keys based on scale itr
+      std::unique_ptr<storage::Tuple> key0(new storage::Tuple(key_schema, true));
+      std::unique_ptr<storage::Tuple> key1(new storage::Tuple(key_schema, true));
+      std::unique_ptr<storage::Tuple> key2(new storage::Tuple(key_schema, true));
+      std::unique_ptr<storage::Tuple> key3(new storage::Tuple(key_schema, true));
+      std::unique_ptr<storage::Tuple> key4(new storage::Tuple(key_schema, true));
+      std::unique_ptr<storage::Tuple> key5(new storage::Tuple(key_schema, true));
+      std::unique_ptr<storage::Tuple> key6(new storage::Tuple(key_schema, true));
+      std::unique_ptr<storage::Tuple> key7(new storage::Tuple(key_schema, true));
+      std::unique_ptr<storage::Tuple> keynonce(new storage::Tuple(key_schema, true));
+
+      key0->SetValue(0, ValueFactory::GetIntegerValue(100 * scale_itr), pool);
+      key0->SetValue(1, ValueFactory::GetStringValue("a"), pool);
+      key1->SetValue(0, ValueFactory::GetIntegerValue(100 * scale_itr), pool);
+      key1->SetValue(1, ValueFactory::GetStringValue("b"), pool);
+      key2->SetValue(0, ValueFactory::GetIntegerValue(100 * scale_itr), pool);
+      key2->SetValue(1, ValueFactory::GetStringValue("c"), pool);
+      key3->SetValue(0, ValueFactory::GetIntegerValue(400 * scale_itr), pool);
+      key3->SetValue(1, ValueFactory::GetStringValue("d"), pool);
+      key4->SetValue(0, ValueFactory::GetIntegerValue(500 * scale_itr), pool);
+      key4->SetValue(1, ValueFactory::GetStringValue(
+          "eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee"
+              "eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee"
+              "eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee"
+              "eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee"
+              "eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee"
+              "eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee"
+              "eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee"
+              "eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee"
+              "eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee"
+              "eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee"
+              "eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee"
+              "eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee"
+              "eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee"
+              "eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee"
+              "eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee"
+              "eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee"
+              "eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee"
+              "eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee"
+              "eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee"
+              "eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee"), pool);
+
+      key5->SetValue(0, ValueFactory::GetIntegerValue(500 * scale_itr), pool);
+      key5->SetValue(1, ValueFactory::GetStringValue("e"), pool);
+
+      key6->SetValue(0, ValueFactory::GetIntegerValue(600 * scale_itr), pool);
+      key6->SetValue(1, ValueFactory::GetStringValue("f"), pool);
+
+      keynonce->SetValue(0, ValueFactory::GetIntegerValue(1000 * scale_itr), pool);
+      keynonce->SetValue(1, ValueFactory::GetStringValue("g"), pool);
+
+      // INSERT
+      index->InsertEntry(key0.get(), item0);
+      index->InsertEntry(key1.get(), item1);
+      index->InsertEntry(key1.get(), item2);
+      index->InsertEntry(key1.get(), item1);
+      index->InsertEntry(key1.get(), item1);
+      index->InsertEntry(key1.get(), item0);
+
+      index->InsertEntry(key2.get(), item1);
+      index->InsertEntry(key3.get(), item1);
+      index->InsertEntry(key4.get(), item2);
+
+      index->InsertEntry(key5.get(), item1);
+      index->InsertEntry(key5.get(), item2);
+
+      index->InsertEntry(key6.get(), item0);
+      index->InsertEntry(key6.get(), item2);
+
+      index->InsertEntry(key7.get(), item0);
+      index->InsertEntry(key7.get(), item0);
+      index->InsertEntry(key7.get(), item1);
+    }
+  }
+
+  TEST(IndexTests, IndexDeltaTest) {
     auto pool = TestingHarness::GetInstance().GetTestingPool();
     std::vector<ItemPointer> locations;
 
@@ -399,7 +478,7 @@ namespace peloton {
 
     // Single threaded test
     size_t scale_factor = 1;
-    LaunchParallelTest(1, SplitInsertTest, index.get(), pool, scale_factor);
+    LaunchParallelTest(1, SplitInsertIndexDeltaTest, index.get(), pool, scale_factor);
     LaunchParallelTest(1, DeleteTest, index.get(), pool, scale_factor);
 
     // Checks
@@ -426,6 +505,44 @@ namespace peloton {
 
     delete tuple_schema;
   }
+
+
+//  TEST(IndexTests, SplitConsolidateTest) {
+//    auto pool = TestingHarness::GetInstance().GetTestingPool();
+//    std::vector<ItemPointer> locations;
+//
+//    // INDEX
+//    std::unique_ptr<index::Index> index(BuildIndex());
+//
+//    // Single threaded test
+//    size_t scale_factor = 1;
+//    LaunchParallelTest(1, SplitInsertTest, index.get(), pool, scale_factor);
+//    LaunchParallelTest(1, DeleteTest, index.get(), pool, scale_factor);
+//
+//    // Checks
+//    std::unique_ptr<storage::Tuple> key0(new storage::Tuple(key_schema, true));
+//    std::unique_ptr<storage::Tuple> key1(new storage::Tuple(key_schema, true));
+//    std::unique_ptr<storage::Tuple> key2(new storage::Tuple(key_schema, true));
+//
+//    key0->SetValue(0, ValueFactory::GetIntegerValue(100), pool);
+//    key0->SetValue(1, ValueFactory::GetStringValue("a"), pool);
+//    key1->SetValue(0, ValueFactory::GetIntegerValue(100), pool);
+//    key1->SetValue(1, ValueFactory::GetStringValue("b"), pool);
+//    key2->SetValue(0, ValueFactory::GetIntegerValue(100), pool);
+//    key2->SetValue(1, ValueFactory::GetStringValue("c"), pool);
+//
+//    locations = index->ScanKey(key0.get());
+//    EXPECT_EQ(locations.size(), 0);
+//
+//    locations = index->ScanKey(key1.get());
+//    EXPECT_EQ(locations.size(), 2);
+//
+//    locations = index->ScanKey(key2.get());
+//    EXPECT_EQ(locations.size(), 1);
+//    EXPECT_EQ(locations[0].block, item1.block);
+//
+//    delete tuple_schema;
+//  }
 
 //TEST(IndexTests, MultiThreadedInsertTest) {
 //  auto pool = TestingHarness::GetInstance().GetTestingPool();
